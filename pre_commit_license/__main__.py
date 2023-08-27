949b90e06dff48a01c4aba0158d59e50c57d50d7
Mert Armagan Sari <mertarmagansari@gmail.com>
2023-08-27 12:33:25 +0300

    chore: update var names

--- pre_commit_license/__main__.py
+++ pre_commit_license/__main__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-# TODO: convert to check and add license for scripts
 from __future__ import annotations
 
 import argparse
 from typing import IO, NamedTuple, Sequence
 
-DEFAULT_PRAGMA = b"# -*- coding: utf-8 -*-"
+# TODO: replace with current year dynamically (if license added?)
+DEFAULT_LICENSE = b'"""(C) Corporate 2023 AG."""'
 
 
 def has_coding(line: bytes) -> bool:
@@ -22,27 +22,28 @@ def has_coding(line: bytes) -> bool:
 class ExpectedContents(NamedTuple):
     shebang: bytes
     rest: bytes
-    # True: has exactly the coding pragma expected
-    # False: missing coding pragma entirely
-    # None: has a coding pragma, but it does not match
-    pragma_status: bool | None
+    # True: has exactly the license header expected
+    # False: missing license header entirely
+    # None: has a license header, but it does not match
+    license_status: bool | None
     ending: bytes
 
     @property
-    def has_any_pragma(self) -> bool:
-        return self.pragma_status is not False
+    def has_any_license(self) -> bool:
+        return self.license_status is not False
 
-    def is_expected_pragma(self, remove: bool) -> bool:
-        expected_pragma_status = not remove
-        return self.pragma_status is expected_pragma_status
+    def is_expected_license(self, remove: bool) -> bool:
+        expected_license_status = not remove
+        return self.license_status is expected_license_status
 
 
 def _get_expected_contents(
     first_line: bytes,
     second_line: bytes,
     rest: bytes,
-    expected_pragma: bytes,
+    expected_license: bytes,
 ) -> ExpectedContents:
+    # TODO: add docstring match
     ending = b"\r\n" if first_line.endswith(b"\r\n") else b"\n"
 
     if first_line.startswith(b"#!"):
@@ -53,38 +54,38 @@ def _get_expected_contents(
         potential_coding = first_line
         rest = second_line + rest
 
-    if potential_coding.rstrip(b"\r\n") == expected_pragma:
-        pragma_status: bool | None = True
+    if potential_coding.rstrip(b"\r\n") == expected_license:
+        license_status: bool | None = True
     elif has_coding(potential_coding):
-        pragma_status = None
+        license_status = None
     else:
-        pragma_status = False
+        license_status = False
         rest = potential_coding + rest
 
     return ExpectedContents(
         shebang=shebang,
         rest=rest,
-        pragma_status=pragma_status,
+        license_status=license_status,
         ending=ending,
     )
 
 
-def fix_encoding_pragma(
+def fix_license_header(
     f: IO[bytes],
     remove: bool = False,
-    expected_pragma: bytes = DEFAULT_PRAGMA,
+    expected_license: bytes = DEFAULT_LICENSE,
 ) -> int:
     expected = _get_expected_contents(
         f.readline(),
         f.readline(),
         f.read(),
-        expected_pragma,
+        expected_license,
     )
 
     # Special cases for empty files
     if not expected.rest.strip():
         # If a file only has a shebang or a coding pragma, remove it
-        if expected.has_any_pragma or expected.shebang:
+        if expected.has_any_license or expected.shebang:
             f.seek(0)
             f.truncate()
             f.write(b"")
@@ -92,7 +93,7 @@ def fix_encoding_pragma(
         else:
             return 0
 
-    if expected.is_expected_pragma(remove):
+    if expected.is_expected_license(remove):
         return 0
 
     # Otherwise, write out the new file
@@ -100,55 +101,55 @@ def fix_encoding_pragma(
     f.truncate()
     f.write(expected.shebang)
     if not remove:
-        f.write(expected_pragma + expected.ending)
+        f.write(expected_license + expected.ending)
     f.write(expected.rest)
 
     return 1
 
 
-def _normalize_pragma(pragma: str) -> bytes:
-    return pragma.encode().rstrip()
+def _normalize_license(license: str) -> bytes:
+    return license.encode().rstrip()
 
 
 def main(argv: Sequence[str] | None = None) -> int:
     parser = argparse.ArgumentParser(
-        "Fixes the encoding pragma of python files",
+        "Fixes the license header of python files",
     )
     parser.add_argument("filenames", nargs="*", help="Filenames to fix")
     parser.add_argument(
-        "--pragma",
-        default=DEFAULT_PRAGMA,
-        type=_normalize_pragma,
-        help=(f"The encoding pragma to use.  " f"Default: {DEFAULT_PRAGMA.decode()}"),
+        "--license",
+        default=DEFAULT_LICENSE,
+        type=_normalize_license,
+        help=(f"The license header to use.  " f"Default: {DEFAULT_LICENSE.decode()}"),
     )
     parser.add_argument(
         "--remove",
         action="store_true",
-        help="Remove the encoding pragma (Useful in a python3-only codebase)",
+        help="Remove the license header",
     )
     args = parser.parse_args(argv)
 
-    retv = 0
+    return_value = 0
 
     if args.remove:
-        fmt = "Removed encoding pragma from {filename}"
+        message = "Removed the license header from {filename}"
     else:
-        fmt = "Added `{pragma}` to {filename}"
+        message = "Added `{license}` to {filename}"
 
     for filename in args.filenames:
         with open(filename, "r+b") as f:
-            file_ret = fix_encoding_pragma(
+            file_return_value = fix_license_header(
                 f,
                 remove=args.remove,
-                expected_pragma=args.pragma,
+                expected_license=args.license,
             )
-            retv |= file_ret
-            if file_ret:
+            return_value |= file_return_value
+            if file_return_value:
                 print(
-                    fmt.format(pragma=args.pragma.decode(), filename=filename),
+                    message.format(license=args.license.decode(), filename=filename),
                 )
 
-    return retv
+    return return_value
 
 
 if __name__ == "__main__":
